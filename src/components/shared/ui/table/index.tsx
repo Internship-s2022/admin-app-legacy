fe84e69b1033ee0714bfdc958c1dabfd49daf909
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-09-27 15:48:38 -0300

    RA-5: Created Table component and it's interface

---
+++ src/components/shared/ui/table/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Table as BasicTable,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+
+import { TableProps } from './types';
+
+const Table = (props: TableProps): JSX.Element => {
+  const { headers, value } = props;
+  return (
+    <TableContainer>
+      <BasicTable>
+        <TableHead>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableCell key={index}>{header}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            {value.map((cell, index) => (
+              <TableCell key={index}>{cell}</TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </BasicTable>
+    </TableContainer>
+  );
+};
+
+export default Table;
