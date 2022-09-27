94f3e93dafa9bd2cde223d16b8d9851b27cd845a
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-09-27 12:52:45 -0300

    Finished implementing modal and button on home

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from 'src/components/shared/common';
+import { Variant } from 'src/components/shared/ui/button/types';
 import Modal from 'src/components/shared/ui/modal';
 
 import styles from './home.module.css';
@@ -9,7 +10,7 @@ const Home = () => {
   const [open, setOpen] = React.useState(false);
   return (
     <section className={styles.container}>
-      <Button onClick={() => setOpen(true)} label="pum" />
+      <Button materialVariant={Variant.CONTAINED} onClick={() => setOpen(true)} label="pum" />
       <h2>Home screen</h2>
       <Modal onClose={setOpen} isOpen={open} testId="testId">
         <div>
