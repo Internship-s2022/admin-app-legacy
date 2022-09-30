a889c77ce0554ec65d4f193f06fd87fa1dd91cf2
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-09-30 11:30:52 -0300

    RA-45: Applied table to user layout, modified shared component

--- src/components/shared/ui/table/index.tsx
+++ src/components/shared/ui/table/index.tsx
@@ -8,31 +8,58 @@ import {
   TableRow,
 } from '@mui/material';
 
+import { Button } from '../index';
 import styles from './table.module.css';
 import { TableProps } from './types';
 
 const Table = (props: TableProps): JSX.Element => {
-  const { headers, value, testId } = props;
+  const { buttonLabel, buttonVariant, buttonTestId, onClick, applyButton, headers, value, testId } =
+    props;
   return (
-    <TableContainer id={testId}>
-      <BasicTable>
-        <TableHead>
-          <TableRow className={styles.headers}>
-            {headers.map((header, index) => (
-              <TableCell key={index}>{header}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            {value.map((cell, index) => (
-              <TableCell key={index}>{cell}</TableCell>
-            ))}
-          </TableRow>
-        </TableBody>
-      </BasicTable>
-    </TableContainer>
+    <>
+      <TableContainer id={testId}>
+        <BasicTable>
+          <TableHead>
+            <TableRow className={styles.headers}>
+              {headers.map((header, index) => (
+                <TableCell key={index}>{header[index]}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody className={styles.body}>
+            {value.map((row, index) => {
+              if (row)
+                return (
+                  <TableRow key={index} className={styles.row}>
+                    {headers.map((header, index) => {
+                      return <TableCell key={index}>{row[header]}</TableCell>;
+                    })}
+                    {applyButton && (
+                      <TableCell>
+                        <Button
+                          materialVariant={buttonVariant}
+                          onClick={onClick}
+                          label={buttonLabel}
+                          testId={buttonTestId}
+                        />
+                      </TableCell>
+                    )}
+                  </TableRow>
+                );
+            })}
+          </TableBody>
+        </BasicTable>
+      </TableContainer>
+    </>
   );
 };
 
 export default Table;
+
+// {value.map((cell, index) => (
+//   <TableRow className={styles.row} key={index}>
+//     <TableCell>{cell.prop}</TableCell>
+//     {/* <TableCell>{cell.role}</TableCell> */}
+//
+//   </TableRow>
+// ))}
