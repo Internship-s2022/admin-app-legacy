5ccc3030868ea37113c23e78f5b405b277a1601e
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-10-11 12:41:14 -0300

    RA-85: Replaced social media icons with logout icon and span

--- src/components/shared/common/header/index.tsx
+++ src/components/shared/common/header/index.tsx
@@ -11,25 +11,12 @@ const Header = () => {
         <nav className={styles.brand}>
           <Navbar />
         </nav>
-        <div>
-          <a href="https://www.facebook.com/radiumrocket" target="_blank" rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
-            />
-          </a>
-          <a href="https://twitter.com/radiumrocket" target="_blank" rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
-            />
-          </a>
-          <a href="https://www.instagram.com/radium.rocket/" target="_blank" rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
-            />
-          </a>
+        <div className={styles.logout}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
+          </svg>
+          <span>Salir</span>
         </div>
       </div>
       <nav className={styles.navbar}>
