d93abc057c1b8849ef2bd5d2f9e208e0f74b0431
JuanMornik <juancruz.moreira@radiumrocket.com>
2022-10-19 18:31:23 -0300

    loader and modals management globally added. Bug in users modal fixed

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { RootState } from 'src/redux/store';
 
 import { Home, Login, Users } from '../pages';
 import Clients from '../pages/clients';
@@ -7,11 +10,16 @@ import Employees from '../pages/employees';
 import Projects from '../pages/projects';
 import StoryBook from '../pages/storybook';
 import { Header } from '../shared/common';
+import { Loader } from '../shared/ui';
 import styles from './layout.module.css';
 
 const Layout = (): JSX.Element => {
+  const isLoading = useSelector((state: RootState) => state.ui.isLoading);
+
   return (
     <div className={styles.container}>
+      {isLoading && <Loader />}
+
       <BrowserRouter>
         <Header />
         <Routes>
