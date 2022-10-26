1b20323fef35d0193383e023b35930c28d980f59
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-10-26 12:56:41 -0300

    RA-146: Uncommented beforeAll on jestSetup and deleted unnecesary import on tests

--- src/components/pages/storybook/__tests__/index.test.tsx
+++ src/components/pages/storybook/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { fireEvent, getByTestId, render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import StoryBook from 'src/components/pages/storybook';
 import store from 'src/redux/store';
