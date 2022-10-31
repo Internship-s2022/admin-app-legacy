0b521232bce58326e71f82b12461a278bc4ede15
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2022-10-31 09:55:49 -0300

    RA-102: Fixed comments and form in the UI

--- src/components/pages/employees/edit-employee/index.tsx
+++ src/components/pages/employees/edit-employee/index.tsx
@@ -65,185 +65,191 @@ const EditEmployee = () => {
 
   const lastestEmployees = matchedEmployee.slice(-2);
 
+  const onSubmit = () => {
+    console.log('test');
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.welcomeMessage}>Editar un empleado</div>
-      <div className={styles.fieldsContainer}>
-        <div className={styles.secondContainer}>
-          <div className={styles.wrapperContainer}>
-            <div className={styles.textInputContainer}>
-              <div className={styles.textInput}>
-                <TextInput
-                  control={control}
-                  testId={'firstNameInput'}
-                  label="Nombre"
-                  name="firstName"
-                  type={'text'}
-                  variant="outlined"
-                  error
-                  fullWidth
-                />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.welcomeMessage}>Editar un empleado</div>
+        <div className={styles.fieldsContainer}>
+          <div className={styles.secondContainer}>
+            <div className={styles.wrapperContainer}>
+              <div className={styles.textInputContainer}>
+                <div className={styles.textInput}>
+                  <TextInput
+                    control={control}
+                    testId={'firstNameInput'}
+                    label="Nombre"
+                    name="firstName"
+                    type={'text'}
+                    variant="outlined"
+                    error
+                    fullWidth
+                  />
+                </div>
+                <div className={styles.textInput}>
+                  <TextInput
+                    control={control}
+                    testId={'lastNameInput'}
+                    label="Apellido"
+                    name="lastName"
+                    type={'text'}
+                    variant="outlined"
+                    error
+                    fullWidth
+                  />
+                </div>
+                <div className={styles.textInput}>
+                  <TextInput
+                    control={control}
+                    testId={'emailInput'}
+                    label="Email"
+                    name="email"
+                    type={'text'}
+                    variant="outlined"
+                    error
+                    fullWidth
+                  />
+                </div>
               </div>
-              <div className={styles.textInput}>
-                <TextInput
-                  control={control}
-                  testId={'lastNameInput'}
-                  label="Apellido"
-                  name="lastName"
-                  type={'text'}
-                  variant="outlined"
-                  error
-                  fullWidth
-                />
-              </div>
-              <div className={styles.textInput}>
-                <TextInput
-                  control={control}
-                  testId={'emailInput'}
-                  label="Email"
-                  name="email"
-                  type={'text'}
-                  variant="outlined"
-                  error
-                  fullWidth
-                />
+              <div className={styles.textInputContainer}>
+                <div className={styles.textInput}>
+                  <DatePicker
+                    label={'Fecha de cumpleaños'}
+                    testId={'datePickerTestId'}
+                    name="dateOfBirth"
+                    control={control}
+                    error
+                  />
+                </div>
+                <div className={styles.textInput}>
+                  <Dropdown
+                    control={control}
+                    testId={'seniorityDropdown'}
+                    label="Seniority"
+                    name="seniority"
+                    options={seniority}
+                    error
+                    fullWidth
+                  />
+                </div>
+                <div className={styles.textInput}>
+                  <AutocompleteInput control={control} name={'skills'} skills={arraySkills} />
+                </div>
               </div>
             </div>
-            <div className={styles.textInputContainer}>
-              <div className={styles.textInput}>
-                <DatePicker
-                  label={'Fecha de cumpleaños'}
-                  testId={'datePickerTestId'}
-                  name="dateOfBirth"
+            <div className={styles.secondWrapperContainer}>
+              <div className={styles.checkboxContainer}>
+                <div className={styles.titles}>Rol potencial</div>
+                <CheckboxInput
+                  testId={'checkboxInput'}
+                  name="potentialRole"
                   control={control}
-                  error
+                  options={checkboxData}
                 />
               </div>
-              <div className={styles.textInput}>
-                <Dropdown
-                  control={control}
-                  testId={'seniorityDropdown'}
-                  label="Seniority"
-                  name="seniority"
-                  options={seniority}
-                  error
-                  fullWidth
+              <div>
+                <div className={styles.titles}>Disponibilidad</div>
+                <ToggleButton
+                  handleChange={handleToggleChange}
+                  testId="toggleButtonTestId"
+                  checked={selected}
                 />
-              </div>
-              <div className={styles.textInput}>
-                <AutocompleteInput control={control} name={'skills'} skills={arraySkills} />
               </div>
             </div>
           </div>
-          <div className={styles.secondWrapperContainer}>
-            <div className={styles.checkboxContainer}>
-              <div className={styles.titles}>Rol potencial</div>
-              <CheckboxInput
-                testId={'checkboxInput'}
-                name="potentialRole"
-                control={control}
-                options={checkboxData}
-              />
-            </div>
-            <div>
-              <div className={styles.titles}>Disponibilidad</div>
-              <ToggleButton
-                handleChange={handleToggleChange}
-                testId="toggleButtonTestId"
-                checked={selected}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.rightContainer}>
-          <div className={styles.pepito}>
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  {projectHeadersEmp.map((header, index) => {
+          <div className={styles.rightContainer}>
+            <div className={styles.pepito}>
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    {projectHeadersEmp.map((header, index) => {
+                      return (
+                        <th className={styles.header} key={index}>
+                          {header.header}
+                        </th>
+                      );
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {lastestEmployees.map((data) => {
                     return (
-                      <th className={styles.header} key={index}>
-                        {header.header}
-                      </th>
+                      <tr key={data.id}>
+                        {projectHeadersEmp.map((header, index) => {
+                          return (
+                            <td className={styles.rows} key={index}>
+                              {data[header.key]}
+                            </td>
+                          );
+                        })}
+                      </tr>
                     );
                   })}
-                </tr>
-              </thead>
-              <tbody>
-                {lastestEmployees.map((data) => {
-                  return (
-                    <tr key={data.id}>
-                      {projectHeadersEmp.map((header, index) => {
-                        return (
-                          <td className={styles.rows} key={index}>
-                            {data[header.key]}
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-            <div className={styles.viewMore}>
-              <Button
-                testId="viewMoreButton"
-                materialVariant={Variant.TEXT}
-                onClick={() => undefined}
-                label="Ver más"
-              />
+                </tbody>
+              </table>
+              <div className={styles.viewMore}>
+                <Button
+                  testId="viewMoreButton"
+                  materialVariant={Variant.TEXT}
+                  onClick={() => undefined}
+                  label="Ver más"
+                />
+              </div>
             </div>
-          </div>
-          <div className={styles.dropboxContainer}>
-            <div className={styles.rightInputs}>
-              <TextInput
-                control={control}
-                testId={'carreerPlanInput'}
-                label="Plan de Carrera"
-                name="carreerPlan"
-                type={'text'}
-                variant="outlined"
-                error
-                fullWidth
-                multiline
-                maxRows={5}
-              />
-            </div>
-            <div className={styles.rightInputs}>
-              <TextInput
-                control={control}
-                testId={'notesInput'}
-                label="Notas"
-                name="notes"
-                type={'text'}
-                variant="outlined"
-                error
-                fullWidth
-                multiline
-                maxRows={5}
-              />
+            <div className={styles.dropboxContainer}>
+              <div className={styles.rightInputs}>
+                <TextInput
+                  control={control}
+                  testId={'carreerPlanInput'}
+                  label="Plan de Carrera"
+                  name="carreerPlan"
+                  type={'text'}
+                  variant="outlined"
+                  error
+                  fullWidth
+                  multiline
+                  maxRows={5}
+                />
+              </div>
+              <div className={styles.rightInputs}>
+                <TextInput
+                  control={control}
+                  testId={'notesInput'}
+                  label="Notas"
+                  name="notes"
+                  type={'text'}
+                  variant="outlined"
+                  error
+                  fullWidth
+                  multiline
+                  maxRows={5}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.buttonContainer}>
-        <div>
-          <Button
-            testId="cancelButton"
-            materialVariant={Variant.OUTLINED}
-            onClick={() => undefined}
-            label="Cancelar"
-          />
+        <div className={styles.buttonContainer}>
+          <div>
+            <Button
+              testId="cancelButton"
+              materialVariant={Variant.OUTLINED}
+              onClick={() => reset()}
+              label="Cancelar"
+            />
+          </div>
+          <div>
+            <Button
+              testId="confirmButton"
+              materialVariant={Variant.CONTAINED}
+              onClick={() => handleSubmit(onSubmit)}
+              label="Confirmar"
+            />
+          </div>
         </div>
-        <div>
-          <Button
-            testId="confirmButton"
-            materialVariant={Variant.CONTAINED}
-            onClick={() => undefined}
-            label="Confirmar"
-          />
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
