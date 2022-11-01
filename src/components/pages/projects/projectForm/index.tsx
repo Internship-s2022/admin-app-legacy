def54aa4fef0a20282ac1f7323d68c648a656a47
Coco Lobos <nicolas.lobos@radiumrocket.com>
2022-11-01 15:59:54 -0300

    RA-128: Fix some styles on Add Member Form

--- src/components/pages/projects/projectForm/index.tsx
+++ src/components/pages/projects/projectForm/index.tsx
@@ -7,7 +7,7 @@ import { Variant } from 'src/components/shared/ui/buttons/button/types';
 import BellIcon from 'src/components/shared/ui/icons/bellIcon/bellIcon';
 import ClockIcon from 'src/components/shared/ui/icons/clockIcon/clockIcon';
 import { RootState } from 'src/redux/store';
-import { closeModal, openModal } from 'src/redux/ui/actions';
+import { closeModal } from 'src/redux/ui/actions';
 import { AppDispatch } from 'src/types';
 
 import AddMemberForm from './addMember';
@@ -38,21 +38,6 @@ const ProjectForm = () => {
       </div>
       <div>
         <AddNewProject />
-        <div className={styles.emptyMember}>
-          <div>Este proyecto no cuenta con miembros asociados</div>
-          <div className={styles.messageContainer}>
-            <p>Para agregar un nuevo miembro al proyecto,</p>
-            <p>clickee en agregar miembro</p>
-          </div>
-          <div className={styles.addMemberButton}>
-            <Button
-              testId="addMember"
-              materialVariant={Variant.OUTLINED}
-              onClick={() => dispatch(openModal())}
-              label="+ Agregar Miembro"
-            />
-          </div>
-        </div>
       </div>
       <div className={styles.buttonContainer}>
         <div>
@@ -79,13 +64,6 @@ const ProjectForm = () => {
           onClose={() => dispatch(closeModal())}
         >
           <AddMemberForm />
-          {/* <div className={styles.modalContainer}>
-            <div className={styles.headerAddMember}>Agregar miembro al proyecto</div>
-            <div className={styles.contentContainer}>
-              <div className={styles.memberForm}>TO DO Formulario</div>
-              <div className={styles.memberForm}>TO DO Calendario</div>
-            </div>
-          </div> */}
         </Modal>
       </div>
     </div>
