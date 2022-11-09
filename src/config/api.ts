9477adb1e18b8b019a7664b5cc7be737b64271c1
JuanMornik <juancruz.moreira@radiumrocket.com>
2022-11-09 15:58:25 -0300

    RA-164: headers on api config updated to send token for validation middleware

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('token');
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
+    token: token,
   },
 });
 
