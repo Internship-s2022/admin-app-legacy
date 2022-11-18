fea2eccf157f408bb8a3ed125f367ac4df01d764
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-11-18 11:33:30 -0300

    RA-XX: fixed not found img on searchbar

--- src/components/pages/users/index.tsx
+++ src/components/pages/users/index.tsx
@@ -174,7 +174,7 @@ const Users = () => {
                 </div>
                 <div>
                   <img
-                    src={`${process.env.PUBLIC_URL}/assets/images/rafiki.png`}
+                    src={`${process.env.PUBLIC_URL}/assets/images/searchNotFound.png`}
                     alt="Not found"
                   ></img>
                 </div>
