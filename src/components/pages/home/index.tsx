99cd6f4ee9a4c550d7d1c781a34738055aa95bb3
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-12-06 17:48:49 -0300

    RA-199: Created card component and corresponding icons, applied it on homepage to try it out

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,17 +1,84 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import Card from 'src/components/shared/ui/card';
+import { getProjects } from 'src/redux/project/thunk';
 import { RootState } from 'src/redux/store';
+import { AppDispatch, Resources } from 'src/types';
 
+import { Criticality } from '../projects/types';
 import styles from './home.module.css';
 
 const Home = () => {
   const user = useSelector((state: RootState) => state.auth.authUser);
+  const projects = useSelector((state: RootState) => state.project.list);
+  const dispatch: AppDispatch<null> = useDispatch();
+
+  console.log(projects);
+  const members = projects[0]?.members;
+
+  useEffect(() => {
+    dispatch(getProjects());
+  }, []);
 
   return (
-    <section className={styles.container}>
-      {user.name.length ? <h2>Bienvenido {user.name}</h2> : ''}
-    </section>
+    <>
+      <section className={styles.container}>
+        {user.name.length ? <h2>Bienvenido {user.name}</h2> : ''}
+      </section>
+      <div className={styles.cardContainer}>
+        <Card
+          name={'Radium Admin'}
+          resource={Resources.Proyectos}
+          members={members}
+          criticality={Criticality.BAJA}
+          customMessage={'holis holis holis holis'}
+          isCustom
+        />
+        <Card
+          name={'Luchito Alarcón'}
+          resource={Resources.Empleados}
+          notification={'notificación'}
+        />
+        <Card name={'Nombre Cliente'} resource={Resources.Clientes} notification={'notificación'} />
+        <Card
+          name={'Radium Admin'}
+          resource={Resources.Proyectos}
+          members={members}
+          criticality={Criticality.MEDIA}
+          customMessage={'holis holis holis holis'}
+          isCustom
+        />
+        <Card
+          name={'Luchito Alarcón'}
+          resource={Resources.Empleados}
+          notification={'Notificación no custom'}
+        />
+        <Card name={'Nombre Cliente'} resource={Resources.Clientes} notification={'notificación'} />
+        <Card
+          name={'Radium Admin'}
+          resource={Resources.Proyectos}
+          members={members}
+          criticality={Criticality.ALTA}
+          customMessage={'holis holis holis holis'}
+          isCustom
+        />
+        <Card
+          name={'Luchito Alarcón'}
+          resource={Resources.Empleados}
+          customMessage={'holis holis holis holis'}
+          isCustom
+        />
+        <Card name={'Nombre Cliente'} resource={Resources.Clientes} notification={'notificación'} />
+        <Card
+          name={'Luchito Alarcón'}
+          resource={Resources.Empleados}
+          customMessage={'holis holis holis holis '}
+        />
+        <Card name={'Nombre Cliente'} resource={Resources.Clientes} notification={'notificación'} />
+        <Card name={'Nombre Cliente'} resource={Resources.Clientes} notification={'notificación'} />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/shared/ui/card/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Avatar, AvatarGroup } from '@mui/material';
+
+import ClientCardIcon from '../icons/cardIcons/clientCardIcon';
+import EmployeeCardIcon from '../icons/cardIcons/employeeCardIcon';
+import ProjectCardIcon from '../icons/cardIcons/projectCardIcon';
+import TickIcon from '../icons/cardIcons/tickIcon';
+import styles from './card.module.css';
+
+const defineColor = (data) => {
+  switch (data) {
+    case 'Proyectos':
+      return styles.projectTabIcon;
+    case 'Empleados':
+      return styles.employeeTabIcon;
+    case 'Clientes':
+      return styles.clientTabIcon;
+    default:
+      return '';
+  }
+};
+
+const defineCriticality = (data) => {
+  switch (data) {
+    case 'MEDIA':
+      return styles.mediumCriticality;
+    case 'ALTA':
+      return styles.highCriticality;
+    case 'BAJA':
+      return styles.lowCriticality;
+    default:
+      return '';
+  }
+};
+
+const Card = (props: any) => {
+  const { name, resource, members, notification, criticality, customMessage, isCustom } = props;
+  const isProject = !!(resource === 'Proyectos');
+  const isEmployee = !!(resource === 'Empleados');
+  const isClient = !!(resource === 'Clientes');
+  const iconColor = defineColor(resource);
+  const criticalityColor = defineCriticality(criticality);
+  const shownNotification = isCustom ? 'Notificación Personalizada' : notification;
+
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.baseIconTab} ${iconColor}`}>
+        {isProject && <ProjectCardIcon />}
+        {isEmployee && <EmployeeCardIcon />}
+        {isClient && <ClientCardIcon />}
+      </div>
+      <div className={styles.cardContent}>
+        <div>
+          <div className={styles.title}>
+            <div className={styles.nameContainer}>
+              {isEmployee && <Avatar className={styles.avatars} />}
+              <span>{name}</span>
+            </div>
+            <span>{resource}</span>
+          </div>
+          {isProject && (
+            <div className={styles.projectInfo}>
+              <>
+                <div>
+                  <div>
+                    <AvatarGroup className={styles.avatars}>
+                      {members?.map((member: any) => {
+                        return <Avatar key={member._id} />;
+                      })}
+                    </AvatarGroup>
+                  </div>
+                  <span>{members?.length} involucrados</span>
+                </div>
+                <div className={`${styles.criticality} ${criticalityColor}`}>
+                  {criticality.toLowerCase()}
+                </div>
+              </>
+            </div>
+          )}
+        </div>
+        {isCustom && <div className={styles.customMessage}>{customMessage}</div>}
+        <div className={styles.notification}>
+          <p>{shownNotification}</p>
+          <div className={styles.tickIcon}>
+            <TickIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
