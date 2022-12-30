865e6673da41d28cdbba93f6919f7a0434598894
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2022-12-30 10:46:10 -0300

    RA-256: Fixed message on not allowed page

--- src/components/pages/notAllowed/index.tsx
+++ src/components/pages/notAllowed/index.tsx
@@ -8,7 +8,7 @@ const NotAllowed = () => {
       <img src={`${process.env.PUBLIC_URL}/assets/images/notAllowed.png`} alt="Not found"></img>
       <h2>Acceso denegado</h2>
       <p>Usted no posee los permisos necesarios para ingresar</p>
-      <p>Por favor contactarse con el soporte</p>
+      <p>Por favor contactarse con el equipo de soporte</p>
     </section>
   );
 };
