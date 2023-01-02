11415dfbefbb3b3d5705b2386ff529fd2b24225c
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2023-01-02 11:55:35 -0300

    RA-263: Changed messages for automatic notifications

--- src/components/shared/ui/card/constants.ts
+++ src/components/shared/ui/card/constants.ts
@@ -1,8 +1,8 @@
 export const dataCards = [
-  { key: 101, data: 'Empleado sin proyecto' },
-  { key: 102, data: 'Empleado con ausencias' },
-  { key: 201, data: 'Cliente sin proyectos' },
-  { key: 202, data: 'Cliente pronto a finalizar' },
-  { key: 301, data: 'Proyecto sin miembros' },
-  { key: 302, data: 'Proyecto pronto a finalizar' },
+  { key: 101, data: 'Sin proyecto asignado' },
+  { key: 102, data: 'Ausencias programadas pronto' },
+  { key: 201, data: 'Sin proyectos' },
+  { key: 202, data: 'Pronto a finalizar' },
+  { key: 301, data: 'Sin miembros asignados' },
+  { key: 302, data: 'Pronto a finalizar' },
 ];
